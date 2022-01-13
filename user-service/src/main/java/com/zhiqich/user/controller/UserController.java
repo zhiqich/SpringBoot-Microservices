6ccbe61f4e9f4ee3eafc9fecc1f98abeae680328
Zhiqi Chen <63929986+zhiqich@users.noreply.github.com>
2022-01-13 16:03:08 -0500

    Add files via upload

---
+++ user-service/src/main/java/com/zhiqich/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.zhiqich.user.controller;
+
+import com.zhiqich.user.VO.ResponseTemplateVO;
+import com.zhiqich.user.entity.User;
+import com.zhiqich.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+}
